e104eb1c59d7248eff4ecd2d04fdda9d6cb58331
DevKipesa <kymdommie@gmail.com>
2024-07-02 10:49:59 +0300

    few changes

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -4,33 +4,33 @@ export const data = {
         title: "Technical Support -",
         subTitle: "Nairobi Womens Hospital",
         description:
-          " User Experience, Visual Design",
+          " Updating and maintaining the System, Testing and evaluating new technology to ensure efficiency and that the system runs as intended, Monitoring and maintaining computer networks, Teaching the users how to use and interact with the system, Researching trends to stay updated with the latest technology, avail of resources and apply best industry practices.",
       },
       {
         title: "Front-End Developer -",
         subTitle: "Suvence",
         description:
-          "Web development, Databases, Backend developement",
+          "Used extensive practical and theoretical understanding of various coding languages to aid the development of PMS software, Designing the UI/UX of the PMS I was developing, Review the code to check quality and suggest improvements, Maintaining the code, Managed and updated databases using database interactive languages like SQL.",
       },
       {
         title: "Front-End Developer -",
         subTitle: "PCEE",
         description:
-          "Creative Direction Project Management, Team Leading",
+          "Write, clean and dynamic code, leveraging expertise across multipleprogramming languages to meet diverse requirements, Support software code review, system integration and validation in line with company andregulatory requirements, Build and maintain code libraries used to produce consistent and reliable code, Keep the code quality high.",
       },
     ],
     education: [
       {
         title: "Secondary -",
         subTitle: "Kihara Secondary School",
-        // description:
-        //   "10th Grade",
+        description:
+          "2014-2017",
       },
       {
         title: "University -",
         subTitle: "Masinde Muliro University of Science and Technology",
         description:
-          "Second Class - Upper Division",
+          "Second Class (Upper Division) - 2018-2022",
       },
       // {
       //   title: "B.Tech",
